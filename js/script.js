989869c153fb82b83151767e67c7da99f64c6be8
Davron <davron1104@yandex.com>
2020-12-13 21:26:04 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+' use strict ';
+// let number = 4.6;
+// console.log(number);
+// const persone = "Alex";
+// console.log(persone);
+// const bool = false;
+// console.log(bool);
+// const obj = {
+//     name: "Davron",
+//     age: 33, 
+//     isMarried: true
+// }; 
+// // console.log(obj.age);
+// console.log(obj["isMarried"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+// let number = 4;
+//     number = 4.6;
+// console.log(-4/0);
+
+// string
+// const persone = `5`;
+// console.log('alex' + 5);
+// const bool = true;
+
+// let und;
+// console.log(und);
+// const obj = {
+//     name: 'Davron',
+//     age: 33,
+//     isMarried: true
+// };
+
+// console.log(obj['isMarried']);
+// let arr = ['plum.png', 'orange.jpg', 4, 'apple.bmp', {}, []];
+// console.log(arr[0]);
+
+// alert('Hello World'); 
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer =prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Как ваше фамилия?', "");
+// answers[2] = prompt('Сколько вам лет?', "");
+// answers[3] = prompt('Какой ваш адрес?', "");
+
+// document.write(answers);
+
+// Интерполяция (ES5);
+// const category = 'toys';
+
+// Операторы;
+// console.log('arr' + "- object");
+// console.log(4 + "- object");
+// console.log(4 + +"- object");
+// console.log(4 + +"5");
+
+
+// Инкримент и декримент;
+// let incr = 10,
+//     decr = 10;
+
+//     // incr++;
+//     // decr--;
+
+//     console.log(incr++);
+//     console.log(decr--);
+
+    // console.log(5%2);
+
+    // console.log(2*4 == 8);
+
+    console.log(2+2*2 === 8);
+
+      const isChecked = false,
+      isClose = false;
+
+      console.log(isChecked || !isClose);
+      
+
+
+      
+
+
+    
+
+
